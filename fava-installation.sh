253ee99236c7dbe53a42649c6cbe5c5663c35ada
Stephen Hoover <32989727+StephenHoover@users.noreply.github.com>
2020-01-12 16:47:39 -0500

    Create fava-installation.sh

---
+++ fava-installation.sh
@@ -0,0 +1,14 @@
+# Tutorial for Arch Based Linux Systems e.g., Manjaro
+
+## Python 3.7.X Pip
+sudo pacman -S python-pip
+
+## Install Beancount Plain Text Account Software
+pip install beancount --user
+
+## Install fava web-app GUI for Beancount
+pip install fava --user
+
+## To Run
+# cd .../directory-with-ledger/
+# fava filename.beancount
