d088ceb4eee16f1dcde4af279f6ec379cd68ba75
Stephen Hoover <32989727+StephenHoover@users.noreply.github.com>
2020-01-12 14:37:00 -0500

    Add files via upload

---
+++ jekyll-installation.sh
@@ -0,0 +1,24 @@
+sudo pacman -S ruby ruby-rdoc
+
+gem update --user-install
+gem install jekyll --user-install
+
+## finally add $HOME/.gem/ruby/2.6.0/bin to your PATH variable
+## Source: https://wiki.archlinux.org/index.php/ruby#Setup
+
+sudo nano ~/.profile
+
+# Manually Paste the following line:
+  # PATH="$PATH:$(ruby -e 'puts Gem.user_dir')/bin"
+
+## Save Changes
+  # Ctrl X
+  # Y
+
+exit # Restart Terminal
+
+gem env
+gem list
+gem install bundler
+
+reboot
